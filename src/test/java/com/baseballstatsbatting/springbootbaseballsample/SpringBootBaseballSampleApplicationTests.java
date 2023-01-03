b8c4e15abf0ead6510658cc16cf7eb63eaa02107
nmt667r <nm66flat@gmial.com>
2023-01-03 12:30:44 +0900

    first commit

---
+++ src/test/java/com/baseballstatsbatting/springbootbaseballsample/SpringBootBaseballSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseballstatsbatting.springbootbaseballsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseballSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
